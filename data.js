013f484d94a912ab4e46644874868f94cc6f1326
charles udenwoke <charlesudenwoke011@gmail.com>
2026-05-22 23:30:59 -0700

    update on the music player

--- data.js
+++ data.js
@@ -1,34 +1,32 @@
 let songs = [
     {
-        name:'song 1',
-        path:'music/Song 1.mp3',
-        artist: 'artist 1',
-        cover: 'images/cover 1.png'
+        name:'Motion',
+        path:'music/khalid_motion_audio_aac_54215.m4a',
+        artist: ' Khalid',
+        cover: 'images/khalidm.jpg'
+    },
+     {
+        name:'Radiance',
+        path:'music/dave_tems_raindance_lyrics_aac_54559.m4a',
+        artist: 'Dave ft Tems',
+        cover: 'images/Davete.jpg'
     },
     {
-        name:'song 2',
-        path:'music/Song 2.mp3',
-        artist: 'artist 2',
-        cover: 'images/cover 2.png'
+        name:'Moth to a Flame',
+        path:'music/the_weeknd_moth_to_a_flame_hd_audio_aac_54406.m4a',
+        artist: 'The Weeknd',
+        cover: 'images/The weekendImg.jpg'
     },
     {
-        name:'song 3',
-        path:'music/Song 3.mp3',
-        artist: 'artist 3',
-        cover: 'images/cover 3.png'
+        name:'All time low',
+        path:'music/jon_bellion_all_time_low_audio_aac_54754.m4a',
+        artist: 'Jon Bellion',
+        cover: 'images/Jonbe.jpg'
     },
     {
-        name:'song 4',
-        path:'music/Song 4.mp3',
-        artist: 'artist 4',
-        cover: 'images/cover 4.png'
+        name:'Sunflower',
+        path:'music/post_malone_sunflower_lyrics_ft._swae_lee_aac_55156.m4a',
+        artist: 'Post Malone',
+        cover: 'images/Post malone.jpg'
     },
-    {
-        name:'song 5',
-        path:'music/Song 5.mp3',
-        artist: 'artist 5',
-        cover: 'images/cover 5 .png'
-    }
-
-
 ]
